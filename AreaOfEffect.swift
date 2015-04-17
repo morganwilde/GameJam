28c0954fbd95371cfd5916ea2023e1c2be6b6d5c
AroSaneD <mail.junk.3135@gmail.com>
2015-04-18 00:39:10 +0300

    Started the areaofeffect class

--- AreaOfEffect.swift
+++ AreaOfEffect.swift
@@ -11,6 +11,28 @@ import SpriteKit
 //An object representing the are in which objects are to be affected
 class AreaOfEffect: SKSpriteNode{
     
+    var effect: Effect?
     
+    
+    init(effect:Effect){
+        super.init(texture: nil, color: SKColor.greenColor(), size: CGSize(width: 400, height: 400))
+        
+        physicsBody = SKPhysicsBody(circleOfRadius: 200)
+        
+        physicsBody?.restitution = 0
+        physicsBody?.dynamic = true
+        physicsBody?.affectedByGravity = false
+        
+        alpha = 0.3
+        
+    }
+    
+    func applyTo(target:SKNode){
+        effect?.applyEffect(target)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
     
 }
