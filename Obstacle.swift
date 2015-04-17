b0fe3b4aa9c29c0aa901369dd3bc8e931e59f828
Henrikas <gyngga@gmail.com>
2015-04-17 19:59:37 +0300

    Obstacle object created

---
+++ Obstacle.swift
@@ -0,0 +1,14 @@
+//
+//  Obstacle.swift
+//  GameJam
+//
+//  Created by Henrikas Jasiūnas on 4/17/15.
+//  Copyright (c) 2015 Henrikas Jasiūnas. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class Obstacle : SKSpriteNode {
+    
+}
